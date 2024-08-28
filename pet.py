45c4c66accf9ca257b4f4f3b7464b6d45289d2c5
PedroPrebeck <pedroazevedo252@gmail.com>
2024-08-28 04:53:36 -0300

    style: :art: reformat code to comply with black formatting standards

--- pet.py
+++ pet.py
@@ -1,26 +1,34 @@
 import sqlite3
 
+
 def create_connection():
-  conn = sqlite3.connect('pets.db', check_same_thread=False)
-  return conn
+    conn = sqlite3.connect("pets.db", check_same_thread=False)
+    return conn
+
 
 def create_table(conn):
-  try:
-    cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS pets
-                      (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, idade INTEGER, peso REAL)''')
-    conn.commit()
-  except sqlite3.Error as e:
-    print(f"Error creating table: {e}")
-    
+    try:
+        cursor = conn.cursor()
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS pets
+                      (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, idade INTEGER, peso REAL)"""
+        )
+        conn.commit()
+    except sqlite3.Error as e:
+        print(f"Error creating table: {e}")
+
+
 def insert_pet(conn, nome, idade, peso):
-  try:
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO pets (nome, idade, peso) VALUES (?, ?, ?)", (nome, idade, peso))
-    conn.commit()
-  except sqlite3.Error as e:
-    print(f"Error inserting pet: {e}")
-    
+    try:
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO pets (nome, idade, peso) VALUES (?, ?, ?)", (nome, idade, peso)
+        )
+        conn.commit()
+    except sqlite3.Error as e:
+        print(f"Error inserting pet: {e}")
+
+
 def fetch_all_pets(conn):
     try:
         cursor = conn.cursor()
